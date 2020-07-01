a2c31db4942495d1fd2562d90331db27cdfbc2f9
farrukh12345 <farrukh.sharipov99@mail.ru>
2020-07-01 07:56:12 +0300

    Initial commit

---
+++ src/ImageClass.java
@@ -0,0 +1,179 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.stream.IntStream;
+
+public class ImageClass {
+    public static int getCompareIndexOfSubImages(BufferedImage img1,
+                                                 BufferedImage img2, int offsetX, int positionY) {
+        BufferedImage img11 = img1.getSubimage(offsetX, positionY,
+                img2.getWidth(), img2.getHeight());
+        img11 = binarize(img11);
+        // Это для повышения точности распознавания, т.к. некоторые карты имею
+        // затененную поверхность
+        changeColor(img11, Constants.ARRAY_RGB_OF_SHADOW_CARD_SURFACE,
+                Constants.ARRAY_RGB_OF_NORMAL_CARD_SURFACE);
+        return Double.valueOf(getDifferencePercent(img11, img2)).intValue();
+
+    }
+
+    private static double getDifferencePercent(BufferedImage img1, BufferedImage img2) {
+        int w1 = img1.getWidth();
+        int h1 = img1.getHeight();
+
+        long diff = 0;
+        for (int y = 0; y < h1; y++) {
+            for (int x = 0; x < w1; x++) {
+                diff += getPixelDifferenc(img1.getRGB(x, y), img2.getRGB(x, y));
+            }
+        }
+        long totalPixels = 3L * 255 * w1 * h1;
+        return 100.0 * diff / totalPixels;
+    }
+
+    private static int getPixelDifferenc(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xff;
+        int g1 = (rgb1 >> 8) & 0xff;
+        int b1 = rgb1 & 0xff;
+        int r2 = (rgb2 >> 16) & 0xff;
+        int g2 = (rgb2 >> 8) & 0xff;
+        int b2 = rgb2 & 0xff;
+        return Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+    }
+
+    private static void changeColor(BufferedImage img, int[] rgb1, int[] rgb2) {
+        int MASK = 0x00ffffff;
+        int sourceRgb = rgb1[0] << 16 | rgb1[1] << 8 | rgb1[2];
+        int destRgb = sourceRgb ^ (rgb1[0] << 16 | rgb1[1] << 8 | rgb1[2]);
+        int w = img.getWidth();
+        int h = img.getHeight();
+        int[] arrRgb = img.getRGB(0, 0, w, h, null, 0, w);
+        for (int i = 0; i < arrRgb.length; i++) {
+            if ((arrRgb[i] & MASK) == sourceRgb) {
+                arrRgb[i] ^= destRgb;
+            }
+        }
+        img.setRGB(0, 0, w, h, arrRgb, 0, w);
+    }
+
+
+    //Функция преобразует изображения в оттенки серого
+    private static BufferedImage grayscale(BufferedImage img) {
+        int alpha, red, green, blue;
+        int newPixel;
+
+        BufferedImage lum = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        for(int i=0; i<img.getWidth(); i++) {
+            for(int j=0; j<img.getHeight(); j++) {
+                //Разбор пикселей на цвета и прозрачность
+                alpha = new Color(img.getRGB(i, j)).getAlpha();
+                red = new Color(img.getRGB(i, j)).getRed();
+                green = new Color(img.getRGB(i, j)).getGreen();
+                blue = new Color(img.getRGB(i, j)).getBlue();
+
+                red = (int) (0.21 * red + 0.71 * green + 0.07 * blue);
+                //Обратно к оригиналу
+                newPixel = colorToRGB(alpha, red, red, red);
+                lum.setRGB(i, j, newPixel);
+            }
+        }
+        return lum;
+    }
+
+    //Гистограмма
+    private static int[] getHistogram(BufferedImage img) {
+        int[] hist = IntStream.range(0, 256).map(i->0).toArray();
+        for(int i=0; i<img.getWidth(); i++) {
+            for(int j=0; j<img.getHeight(); j++) {
+                int red = new Color(img.getRGB (i, j)).getRed();
+                hist[red]++;
+            }
+        }
+        return hist;
+    }
+
+
+    //Получить пороговое значения для бинеаризиции
+    private static int treshold(BufferedImage img) {
+
+        int[] histogram = getHistogram(img);
+        int total = img.getHeight() * img.getWidth();
+
+        float sum = 0;
+        for(int i=0; i<256; i++) sum += i * histogram[i];
+
+        float sumB = 0;
+        int wB = 0;
+        int wF = 0;
+
+        float varMax = 0;
+        int threshold = 0;
+
+        for(int i=0 ; i<256 ; i++) {
+            wB += histogram[i];
+            if(wB == 0) continue;
+            wF = total - wB;
+
+            if(wF == 0) break;
+
+            sumB += (float) (i * histogram[i]);
+            float mB = sumB / wB;
+            float mF = (sum - sumB) / wF;
+
+            float varBetween = (float) wB * (float) wF * (mB - mF) * (mB - mF);
+
+            if(varBetween > varMax) {
+                varMax = varBetween;
+                threshold = i;
+            }
+        }
+
+        return threshold;
+
+    }
+
+    public static BufferedImage binarize(BufferedImage img) {
+
+        int red;
+        int newPixel;
+
+        img = grayscale(img);
+
+        int threshold = treshold(img);
+
+        BufferedImage binarized = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        for(int i=0; i<img.getWidth(); i++) {
+            for(int j=0; j<img.getHeight(); j++) {
+
+                red = new Color(img.getRGB(i, j)).getRed();
+                int alpha = new Color(img.getRGB(i, j)).getAlpha();
+                if(red > threshold) {
+                    newPixel = 255;
+                }
+                else {
+                    newPixel = 0;
+                }
+                newPixel = colorToRGB(alpha, newPixel, newPixel, newPixel);
+                binarized.setRGB(i, j, newPixel);
+
+            }
+        }
+
+        return binarized;
+
+    }
+
+    private static int colorToRGB(int alpha, int red, int green, int blue) {
+
+        int newPixel = 0;
+        newPixel += alpha;
+        newPixel = newPixel << 8;
+        newPixel += red; newPixel = newPixel << 8;
+        newPixel += green; newPixel = newPixel << 8;
+        newPixel += blue;
+
+        return newPixel;
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,115 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Test {
+    private static Map<String, BufferedImage> mapRankTempls = new HashMap<>();
+    private static Map<String, BufferedImage> mapSuitsTempls = new HashMap<>();
+    //Всего обнаружено карт во всех найденных файлах
+    private static int totalFoundCards = 0;
+    //Всего обнаружено карт во всех найденных файлах
+    private static int totalNotFoundCards = 0;
+    //Функция получения Map с соответствующими шаблонами
+    private static Map<String, BufferedImage> loadTemplates(String[] templs)  {
+        return Arrays.stream(templs).collect(Collectors
+                .toMap(s->s.replaceFirst("[.][^.]+$", ""), //Избавляемся от расширения в имени файла
+                        s->{
+                            try
+                            {
+                                return ImageIO.read(Test.class.getClassLoader().getResourceAsStream(s));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                ));
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        mapRankTempls = loadTemplates(Constants.RANK_TEMPLS);
+        if (mapRankTempls == null || mapRankTempls.isEmpty()) {
+            System.out.println("Отсутствуют шаблоны рангов карт ");
+            System.exit(0);
+        };
+        mapSuitsTempls = loadTemplates(Constants.SUITS_TEMPLS);
+        if (mapRankTempls == null || mapRankTempls.isEmpty()) {
+            System.out.println("Отсутствуют шаблоны мастей карт");
+            System.exit(0);
+        }
+        String dirScan = args.length == 0? Constants.DEFAULT_INPUT_DIR
+                : Optional.ofNullable(args[0]).orElse(Constants.DEFAULT_INPUT_DIR);
+        File[] arrlf = new File(dirScan).listFiles((d, n) -> n.endsWith(".png"));
+        if (arrlf.length == 0) {
+            System.out.println("В папке "+dirScan+" файлов с картинками не найдено");
+            System.exit(0);
+        }
+        for (File inFile : arrlf) {
+            BufferedImage img1 = ImageIO.read(inFile);
+            if (img1.getWidth()!= Constants.IMAGE_WIDTH || img1.getHeight() != Constants.IMAGE_HEIGHT){
+                System.out.println("Неверный размерисходного изображения в файле "+inFile.getName());
+                continue;
+            }
+            //массивы для хранения рангов и мастей
+            String[] foundRank = {"?","?","?","?","?"};
+            String[] foundSuits = {"?","?","?","?","?"};
+            for(int cardNum = 0; cardNum < Constants.ARRAY_OF_CARD_POSITION_X.length; cardNum++) {
+                //обработка ранга карты
+                for(Map.Entry<String, BufferedImage> entry : mapRankTempls.entrySet()) {
+                    BufferedImage img2 = entry.getValue();
+                    int offset = Constants.OFFSET_OF_CARD_RANK_X;
+                    String nameRank = entry.getKey();
+                    //Некоторые шаблоны могут в имени файла включать и смещение
+                    //В таком случае вместо смещения по умолчанию из имени файла выделяется
+                    //новый размер смещения для данного шаблона
+                    String[] arrSplitRank = entry.getKey().split("_");
+                    if (arrSplitRank.length == 2) {
+                        nameRank = arrSplitRank[0];
+                        offset = Integer.valueOf(arrSplitRank[1]);
+                    }
+                    int pd = ImageClass.getCompareIndexOfSubImages(img1, img2,
+                            Constants.ARRAY_OF_CARD_POSITION_X[cardNum]+offset,
+                            Constants.CARD_POSITION_RANK_Y);
+                    if (pd <= Constants.ALLOW_RANK_SCALE){
+                        foundRank[cardNum] = nameRank;
+                    }
+                }
+                //обработка масти карты
+                for(Map.Entry<String, BufferedImage> entry : mapSuitsTempls.entrySet()) {
+                    BufferedImage img2 = entry.getValue();
+                    int pd = ImageClass.getCompareIndexOfSubImages(img1, img2,
+                            Constants.ARRAY_OF_CARD_POSITION_X[cardNum]+Constants.OFFSET_OF_CARD_SUITS_X,
+                            Constants.CARD_POSITION_SUITS_Y);
+                    if (pd <= Constants.ALLOW_SUITS_SCALE){
+                        foundSuits[cardNum] = entry.getKey();
+                        //Так масти распознаются точнее, то количество карт считаем по дайденным мастям
+                        totalFoundCards++;
+                    }
+                }
+            }
+            //Собираем строку из массивов найденных рангов и мастей
+            //Если распознать ранг карты не удалось в выходной строке будет знак вопроса
+            String res = IntStream.range(0, foundRank.length)
+                    .mapToObj(i->foundRank[i]+foundSuits[i])
+                    .collect(Collectors.joining())
+                    .replace("??", "");
+            //Считаем нераспознанные карты
+            totalNotFoundCards += res.chars().filter(ch -> ch == '?').count();
+            //Выводим информацию о распознанных картах по данному файлу
+            System.out.println(inFile.getName()+ " - " + res);
+
+        }
+        System.out.println();
+        System.out.printf("Общее количество распознанных карт - %d \n", totalFoundCards);
+        System.out.printf("Общее количество нераспознанных карт - %d \n", totalNotFoundCards);
+        System.out.println("Завершение работы.");
+    }
+}
